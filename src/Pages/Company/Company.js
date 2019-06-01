bdc8aa801e0e2e142adb5587431af832571c1d0e
Saloni Mohta <salonimohta37@gmail.com>
2019-06-01 16:43:00 +0530

    meta keywords added, robots.txt added

--- src/Pages/Company/Company.js
+++ src/Pages/Company/Company.js
@@ -5,6 +5,10 @@ import PastRecruiter from "./PastRecruiter";
 import ReachUs from "./ReachUs";
 
 class ForCompanies extends React.Component {
+  componentDidMount(){
+    document.querySelector("meta[name='keywords']").content="Companies, IIT(ISM) Dhanbad, IIT, Placement, Register company, Recruiters, Brochure, Reach IIT(ISM) Dhanbad";
+    document.querySelector("meta[name='description']").content="Registration of companies for placement in IIT(ISM) Dhanbad";
+  }
   render() {
     return (
       <div>
