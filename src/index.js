53cef3dcf369978f184cd293a14e4b6029b5a418
unknown <asd.shivam98@gmail.com>
2019-09-29 17:49:22 +0530

    added recuriters names and changed image

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./Pages/Home/Home";
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import DummyLink from './Components/DummyLink'
 import ForCompanies from "./Pages/Company/Company";
 import Alumni from "./Pages/Alumni/Alumni";
 import Policy from "./Pages/Policies/Policy";
 import ContactUs from "./Pages/ContactUs/ContactUs";
-<<<<<<< HEAD
-import PlacementStats from "./Pages/PlacementStats/Placement-Stats";
-=======
 import Placement from "./Pages/Placement/Placement";
 import example from "./Pages/example";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
->>>>>>> 28a622f30265aa36f03ae6132ef58aa687251f6a
+
 
 const routing = (
   <Router>
@@ -27,13 +25,10 @@ const routing = (
         <Route exact path="/for-alumni" component={Alumni} />
         <Route exact path="/policies" component={Policy} />
         <Route exact path="/contact-us" component={ContactUs} />
-<<<<<<< HEAD
-        <Route exact path="/placement-stats" component={PlacementStats} />
+        <Route exact path="/placement-stats" component={Placement} />
         <Route exact path="/dummylink" component={DummyLink} />
         <Route component={DummyLink} />
-=======
         <Route exact path="/placement-stats" component={Placement} />
->>>>>>> 28a622f30265aa36f03ae6132ef58aa687251f6a
       </Switch>
       <Footer />
     </div>
